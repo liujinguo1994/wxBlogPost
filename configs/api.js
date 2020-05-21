ba639e48d7f81f67da897dfa1bd18e3dacdca8ea
liujinguo1994 <344009799@qq.com>
2020-05-21 21:38:29 +0800

    域名
    域名修改

--- configs/api.js
+++ configs/api.js
@@ -1,7 +1,7 @@
 //本地开发环境
-var WxApiRoot = 'http://localhost:8080/weChat/';
+var WxApiRoot = 'http://28167q5i65.zicp.vip';
 
 module.exports = {
-  BlogListUrl:WxApiRoot + 'blog/page', // 首页数据
-  BlogDetailsUrl:WxApiRoot + 'blog/article'// 获取博文详情
+  BlogListUrl:WxApiRoot + '/weChat/blog/page', // 首页数据
+  BlogDetailsUrl:WxApiRoot + '/weChat/blog/article'// 获取博文详情
 }
